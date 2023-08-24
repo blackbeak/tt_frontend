de6ca0aa84b64c5db7fc61c7c701c314ac2eb1ba
Steve Jackson <stevejackson@Steves-MacBook-Pro-3.local>
2023-08-24 19:30:44 +0300

    added netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,15 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
-        accessToken: "9a065c20476add1c8c2677508fe565c4a49e4e4fc164494baaa2847a72e50e39cdc850cb9fe66bd1cf046fa21242186fd0125a1a105804860624c4bfefeb3c020e04bf8492a13ddeb5c31b646d8b6ced4b1eb0e157f7e6e6ce8da056630b053a6a7aab3fac2da94c08c5f0b80b8eef4a65336838d7c2b60c8aca7a601ec86786",
+        // LocalHost
+        //apiURL: "http://localhost:1337",
+        //accessToken: "9a065c20476add1c8c2677508fe565c4a49e4e4fc164494baaa2847a72e50e39cdc850cb9fe66bd1cf046fa21242186fd0125a1a105804860624c4bfefeb3c020e04bf8492a13ddeb5c31b646d8b6ced4b1eb0e157f7e6e6ce8da056630b053a6a7aab3fac2da94c08c5f0b80b8eef4a65336838d7c2b60c8aca7a601ec86786",
+        
+        //Railway 
+        
+        apiURL: "https://ttbackend-production-d6df.up.railway.app",
+        accessToken: "59875099e43a4fd50e3835d6bdbf7fea0086b020821151cc636a3fc360cde7acc69043c038167f36d22c1b85a0a13af788265c339b1b49cba8614d334af5ac46d7effff6607d2267494048b3e91926666b7ad8ce059f5674c3e3cc0becd5f2f1ecb76d06edd2abe14d6d635de1043c0fa11f4beb91158a61f93f961f727d0415",
+        
         collectionTypes: [
           {
             singularName: "article",
@@ -81,6 +88,7 @@ module.exports = {
       },
     },
     "gatsby-plugin-image",
+    "gatsby-plugin-netlify",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
